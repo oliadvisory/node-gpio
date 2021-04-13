306dc69551fd9842c554c3f3f221e2d4482068dc
Chase Oliphant <chaseoliphant@gmail.com>
2021-04-13 16:54:00 -0700

    update readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# GPIO using nodejs, typescript & pm2 
+
+```
+# start script
+~ npm start
+```
+
+GPIO library used https://www.npmjs.com/package/onoff see wiring diagram below for this specific example:
+
+
+![Wiring Diagram](https://raw.githubusercontent.com/fivdi/onoff/master/examples/light-switch.png?raw=true)
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,54 +1,30 @@
-// var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-// var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
-// var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
+import * as onoff from "onoff";
+import * as admin from "firebase-admin";
+class Main {
+  constructor() {
+    this.start();
+  }
 
-// function blinkLED() { //function to start blinking
-//   if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
-//     LED.writeSync(1); //set pin state to 1 (turn LED on)
-//   } else {
-//     LED.writeSync(0); //set pin state to 0 (turn LED off)
-//   }
-// }
+  async start() {
+    admin.initializeApp({
+      credential: admin.credential.applicationDefault(),
+      // IMPORTANT: use development database for testing otherwise
+      // you accidentally overwrite your production data
+      databaseURL: process.env.firebase_database_url,
+    });
+  }
 
-// function endBlink() { //function to stop blinking
-//   clearInterval(blinkInterval); // Stop blink intervals
-//   LED.writeSync(0); // Turn LED off
-//   LED.unexport(); // Unexport GPIO to free resources
-// }
+  gpioTest() {
+    const Gpio = onoff.Gpio;
 
-// setTimeout(endBlink, 5000); //stop blinking after 5 seconds
+    const led = new Gpio(17, "out");
+    const button = new Gpio(4, "in", "both");
 
-// const Gpio = require("onoff").Gpio;
+    button.watch((err, value) => {
+      console.log("button pressed");
+      led.writeSync(value);
+    });
+  }
+}
 
-import * as onoff from "onoff"
-const Gpio = onoff.Gpio
-
-const led = new Gpio(17, "out");
-const button = new Gpio(4, "in", "both");
-
-button.watch((err, value) => {
-  console.log("button pressed");
-  led.writeSync(value);
-});
-
-// class Main {
-//   constructor() {
-//     this.start();
-//   }
-
-//   async start() {
-//     let count = 0;
-//     console.log("running node process...");
-//     // setInterval(() => {
-//     //   if (count === 3) {
-//     //     console.log(count);
-//     //     console.log("killing node process...");
-//     //     process.kill(0);
-//     //   }
-//     //   console.log(count);
-//     //   count++;
-//     // }, 1000);
-//   }
-// }
-
-// new Main();
+new Main();
